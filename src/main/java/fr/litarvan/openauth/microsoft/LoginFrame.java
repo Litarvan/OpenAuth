e1d5d0baa5a7571f6887f42ff7dd74892908e95a
Presti <dxssucuk@hotmail.com>
2022-07-11 11:30:09 +0200

    1.1.4
    * Adds an HTTP-Agent in hopes of bypassing the connection termination by Microsoft.
    * Fix crashed on different Versions.

--- src/main/java/fr/litarvan/openauth/microsoft/LoginFrame.java
+++ src/main/java/fr/litarvan/openauth/microsoft/LoginFrame.java
@@ -80,6 +80,7 @@ public class LoginFrame extends JFrame
                 this.future.complete(newValue);
             }
         });
+        webView.getEngine().setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
         webView.getEngine().load(url);
 
         this.setVisible(true);
